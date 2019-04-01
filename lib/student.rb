a6e329527ca9bdec4567e4c9608590b12b6a9248
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-01 23:36:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,15 @@ class Student
   end
 
   def self.create_table
+   sql =  <<-SQL
+     CREATE TABLE songs (
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       grade TEXT
+       )
+       SQL
+   DB[:conn].execute(sql)
+
     
   end
 
